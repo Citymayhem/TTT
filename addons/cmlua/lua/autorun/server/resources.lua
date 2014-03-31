491efe0f94ccfbccc74e1c710db782f368d31d54
Michael Hawkins <admin@badgercode.co.uk>
2014-03-31 10:14:51 +0100

    Removed custom TTT server items
    Removed a number of items that the custom TTT server have but this one won't have.

--- addons/cmlua/lua/autorun/server/resources.lua
+++ addons/cmlua/lua/autorun/server/resources.lua
@@ -1,22 +1,4 @@
 //==
-//Scar20
-//=
-//Material
-resource.AddFile("materials/models/weapons/v_models/go_scar/scope_normal.vtf")
-resource.AddFile("materials/models/weapons/v_models/go_scar/scope_scar20.vtf")
-resource.AddFile("materials/models/weapons/v_models/go_scar/scope_scar20.vmt")
-resource.AddFile("materials/models/weapons/v_models/go_scar/snip_scar20.vmt")
-resource.AddFile("materials/models/weapons/v_models/go_scar/snip_scar20.vtf")
-resource.AddFile("materials/models/weapons/v_models/go_scar/snip_scar20_exponent.vtf")
-//Model
-resource.AddFile("models/weapons/v_snip_scar20.mdl")
-resource.AddFile("models/weapons/v_snip_scar20.sw.vtx")
-resource.AddFile("models/weapons/w_snip_scar20.mdl")
-resource.AddFile("models/weapons/w_snip_scar20.sw.vtx")
-//Sound
-resource.AddFile("sound/scar20_unsil-1.wav")
-
-//==
 //Santa hat
 //=
 //Material
