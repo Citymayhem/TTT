0ded88f68b026bb1d086e2bf6d8bdaf592a5ee7a
Michael Hawkins <BadgerCode@users.noreply.github.com>
2017-03-05 06:06:54 +0000

    Refactors right-click menu (again)

--- addons/cmlua/lua/autorun/client/tttmenu.lua
+++ addons/cmlua/lua/autorun/client/tttmenu.lua
@@ -13,26 +13,22 @@ Adds the right-click context menu to the TTT menu
 ===================================================================================================
 --]]
 hook.Add("TTTScoreboardMenu","cmscoreboardcontextmenu", function(menu)
-	local ply = LocalPlayer()
-	local target = menu.Player
-	
-	PlayClickSound()
-	
-	AddGeneralSection(menu)
-	
-	-- This check is here purely so we can add spacing between the admin and non-admin options
-	-- There's a better way to do this, but I cba
-	local minimumAdminGroup = "mod"
-	if(ply:CheckGroup(minimumAdminGroup)) then
-		menu:AddSpacer()
-		AddAdminSections(menu)
-	end
+	local menuSections = {}
+
+	// Add sections here
+	AddMenuSection(menuSections, BuildGeneralSection())
+	AddMenuSection(menuSections, BuildSpectateSection())
+	AddMenuSection(menuSections, BuildTeleportSection())
+	AddMenuSection(menuSections, BuildSlaySection())
+	AddMenuSection(menuSections, BuildChatSection())
+	AddMenuSection(menuSections, BuildKickSection(menu.Player))
+	AddMenuSection(menuSections, BuildBanSection(menu.Player))
+
+	RenderMenuSections(menu, menuSections)
 end)
 --[[
 ===================================================================================================
 --]]
-
-
 
 
 
@@ -42,80 +38,311 @@ Builds the right click menu
 ===================================================================================================
 --]]
 
-function AddGeneralSection(menu)	
-	AddPlayerMenuOption(menu, "Copy Name", "icon16/user_edit.png", function(target) SetClipboardText(target:Nick()) end)
-	AddPlayerMenuOption(menu, "Copy SteamID", "icon16/tag_blue.png", function(target) SetClipboardText(target:SteamID()) end)
-	AddPlayerMenuOption(menu, "Open Profile", "icon16/world.png", function(target) target:ShowProfile() end)
+function BuildGeneralSection()
+
+	local section = {}
+	table.insert(section, { text = "Copy Name", 
+							icon = "icon16/user_edit.png", 
+							onClick = function(target) SetClipboardText(target:Nick()) end
+							})
+
+	table.insert(section, { text = "Copy SteamID", 
+							icon = "icon16/tag_blue.png", 
+							onClick = function(target) SetClipboardText(target:SteamID()) end
+							})
+
+	table.insert(section, { text = "Open Profile", 
+							icon = "icon16/world.png", 
+							onClick = function(target) target:ShowProfile() end
+							})
+	return section
 end
 
-function AddAdminSections(menu)
 
-	AddULibRestrictedPlayerMenuOption(menu, "Spectate", "icon16/zoom.png", "ulx spectate", function(target) RunConsoleCommand("ulx","spectate", target:Nick())  end)
-	AddULibRestrictedPlayerMenuOption(menu, "Force Spectator", "icon16/status_offline.png", "ulx fspec", function(target) RunConsoleCommand("ulx","spec", target:Nick())  end)
+function BuildSpectateSection()
+
+	local section = {}
 	
-	menu:AddSpacer()
-	
-	AddULibRestrictedPlayerMenuOption(menu, "Teleport To", "icon16/arrow_up.png", "ulx goto", function(target) RunConsoleCommand("ulx","goto", target:Nick())  end)
-	AddULibRestrictedPlayerMenuOption(menu, "Bring (To where you're aiming)", "icon16/arrow_up.png", "ulx teleport", function(target) RunConsoleCommand("ulx","teleport", target:Nick())  end)
-	
-	menu:AddSpacer()
-	
-	AddULibRestrictedPlayerMenuOption(menu, "Slay", "icon16/user_red.png", "ulx slay", function(target) RunConsoleCommand("ulx","slay", target:Nick()) end)
-	AddULibRestrictedPlayerMenuOption(menu, "Slay Next Round(s)", "icon16/clock_red.png", "ulx slaynr", function(target) RunConsoleCommand("ulx","slaynr", target:Nick())  end)
-	
-	menu:AddSpacer()
-	
-	AddULibRestrictedPlayerMenuOption(menu, "Private Message", "icon16/user_comment.png", "ulx psay", function(target) OpenPrivateMessageDialog(target) end)
-	
-	AddULibRestrictedPlayerMenuOption(menu, "Mute", "icon16/comment_delete.png", "ulx mute", function(target) RunConsoleCommand("ulx","mute", target:Nick())  end)
-	AddULibRestrictedPlayerMenuOption(menu, "Unmute", "icon16/comment_add.png", "ulx unmute", function(target) RunConsoleCommand("ulx","unmute", target:Nick())  end)
-	AddULibRestrictedPlayerMenuOption(menu, "Gag", "icon16/sound_mute.png", "ulx gag", function(target) RunConsoleCommand("ulx","gag", target:Nick())  end)
-	AddULibRestrictedPlayerMenuOption(menu, "Ungag", "icon16/sound_low.png", "ulx ungag", function(target) RunConsoleCommand("ulx","ungag", target:Nick())  end)
-	
-	menu:AddSpacer()
-	
-	AddKickSubMenu(menu)
-	
-	menu:AddSpacer()
-	
-	AddBanSubMenu(menu)
+	if PlayerHasRequiredPermission("ulx spectate") then
+		table.insert(section, { text = "Spectate", 
+								icon = "icon16/zoom.png", 
+								onClick = function(target) RunConsoleCommand("ulx","spectate", target:Nick()) end
+								})
+	end
+
+	if PlayerHasRequiredPermission("ulx fspec") then
+		table.insert(section, { text = "Force Spectate", 
+								icon = "icon16/zoom.png", 
+								onClick = function(target) RunConsoleCommand("ulx","fspec", target:Nick()) end
+								})
+	end
+
+	return section
 end
 
-function AddKickSubMenu(menu)
+
+function BuildTeleportSection()
+
+	local section = {}
+	
+	if PlayerHasRequiredPermission("ulx goto") then
+		table.insert(section, { text = "Teleport To", 
+								icon = "icon16/arrow_up.png", 
+								onClick = function(target) RunConsoleCommand("ulx","goto", target:Nick()) end 
+								})
+	end
+
+	if PlayerHasRequiredPermission("ulx teleport") then
+		table.insert(section, { text = "Bring (To where you're aiming)", 
+								icon = "icon16/arrow_up.png", 
+								onClick = function(target) RunConsoleCommand("ulx","teleport", target:Nick()) end 
+								})
+	end
+	return section
+end
+
+
+function BuildSlaySection()
+
+	local section = {}
+	
+	if PlayerHasRequiredPermission("ulx slay") then
+		table.insert(section, { text = "Slay", 
+								icon = "icon16/user_red.png", 
+								onClick = function(target) RunConsoleCommand("ulx","slay", target:Nick()) end 
+								})
+	end
+
+	if PlayerHasRequiredPermission("ulx slaynr") then
+		table.insert(section, { text = "Slay Next Round(s)", 
+								icon = "icon16/clock_red.png", 
+								onClick = function(target) RunConsoleCommand("ulx","slaynr", target:Nick()) end 
+								})
+	end
+	
+	return section
+end
+
+
+function BuildChatSection()
+
+	local section = {}
+	
+	if PlayerHasRequiredPermission("ulx psay") then
+		table.insert(section, { text = "Private Message", 
+								icon = "icon16/user_comment.png", 
+								onClick = function(target) OpenPrivateMessageDialog(target) end 
+								})
+	end
+
+	if PlayerHasRequiredPermission("ulx mute") then
+		table.insert(section, { text = "Mute", 
+								icon = "icon16/comment_delete.png", 
+								onClick = function(target) RunConsoleCommand("ulx","mute", target:Nick()) end 
+								})
+	end
+
+	if PlayerHasRequiredPermission("ulx unmute") then
+		table.insert(section, { text = "Unmute", 
+								icon = "icon16/comment_add.png", 
+								onClick = function(target) RunConsoleCommand("ulx","unmute", target:Nick()) end 
+								})
+	end
+
+	if PlayerHasRequiredPermission("ulx gag") then
+		table.insert(section, { text = "Gag", 
+								icon = "icon16/sound_mute.png", 
+								onClick = function(target) RunConsoleCommand("ulx","gag", target:Nick()) end 
+								})
+	end
+
+	if PlayerHasRequiredPermission("ulx ungag") then
+		table.insert(section, { text = "Ungag", 
+								icon = "icon16/sound_low.png", 
+								onClick = function(target) RunConsoleCommand("ulx","ungag", target:Nick()) end 
+								})
+	end
+	
+	return section
+end
+
+
+function BuildKickSection(target)
+
+	if not PlayerHasRequiredPermission("ulx kick") then
+		return {}
+	end
+
 	-- If the player disconnects, this should fail gracefully. We need to record their name and steam Id in-case this happens for the error message
-	local target = menu.Player
-	local onPlayerDisconnect = function() LocalPlayer():ChatPrint(target:Nick() .. " (" .. target:SteamId() .. ") disconnected before you could kick them.") end
-
-	local kickSubMenu = AddULibRestrictedSubMenu(menu, "Kick", "icon16/door_in.png", "ulx kick")
-	
-	if kickSubMenu == nil then return end
-	
-	AddMenuOption(kickSubMenu, "AFK", "", function(menu) 
-		TryRunPlayerMenuOption(menu.Player, function(target) RunConsoleCommand("ulx", "kick", target:Nick(), "AFK") end, onPlayerDisconnect)
-	end)
-	AddMenuOption(kickSubMenu, "Final warning", "", function(menu)
-		TryRunPlayerMenuOption(menu.Player, function(target) RunConsoleCommand("ulx", "kick", target:Nick(), "Final warning") end, onPlayerDisconnect)
-	end)
-	AddMenuOption(kickSubMenu, "Other (specify)", "icon16/textfield.png", function(menu)
-		TryRunPlayerMenuOption(menu.Player, function(target) OpenKickReasonDialog(target, target:Nick(), onPlayerDisconnect) end, onPlayerDisconnect)
-	end)
-end
-
-function AddBanSubMenu(menu)
-	-- Even if the player disconnects, we want the ban to succeed
-	local target = menu.Player
+	local targetName = target:Nick()
 	local targetSteamId = target:SteamID()
-	
-	local banSubMenu = AddULibRestrictedSubMenu(menu, "Ban", "icon16/stop.png", "ulx banid")
-	
-	if banSubMenu == nil then return end
-	
-	AddMenuOption(banSubMenu, "Ghosting (Permanent)", "", function(target) RunConsoleCommand("ulx", "banid", targetSteamId, "1w", "Ghosting")  end )
-	AddMenuOption(banSubMenu, "Hacking (Permanent)", "", function(target) RunConsoleCommand("ulx", "banid", targetSteamId, "1w", "Hacking")  end )
-	AddMenuOption(banSubMenu, "Ignored previous warnings (1 week)", "", function(target) RunConsoleCommand("ulx", "banid", targetSteamId, "1w", "Ignored previous warnings")  end )
-	AddMenuOption(banSubMenu, "Mass RDM (Permanent)", "", function(target) RunConsoleCommand("ulx", "ban", targetSteamId, "0", "Mass RDM")  end )
-	AddMenuOption(banSubMenu, "Other (specify)", "icon16/textfield.png", function(menu) OpenBanPlayerDialog(target:Nick(), targetSteamId) end)
+	local onPlayerDisconnect = function() LocalPlayer():ChatPrint(targetName .. " (" .. targetSteamId .. ") disconnected before you could kick them.") end
+
+	local kickMenuItem = {
+		text = "Kick",
+		icon = "icon16/door_in.png",
+		subMenu = {}
+	}
+
+	table.insert(kickMenuItem.subMenu, { text = "AFK",
+										 onClick = function(target) RunConsoleCommand("ulx", "kick", targetName, "AFK") end,
+										 onFail = onPlayerDisconnect
+										 })
+
+	table.insert(kickMenuItem.subMenu, { text = "Final warning",
+										 onClick = function(target) RunConsoleCommand("ulx", "kick", targetName, "Final warning") end,
+										 onFail = onPlayerDisconnect
+										 })
+
+	table.insert(kickMenuItem.subMenu, { text = "Other (specify)",
+										 icon = "icon16/textfield.png",
+										 onClick = function(target) OpenKickReasonDialog(target, targetName, onPlayerDisconnect) end,
+										 onFail = onPlayerDisconnect
+										 })
+
+	return { kickMenuItem }
 end
+
+
+function BuildBanSection(target)
+
+	if not PlayerHasRequiredPermission("ulx banid") then
+		return {}
+	end
+
+	-- Even if the player disconnects, we want the ban to succeed
+	local targetName = target:Nick()
+	local targetSteamId = target:SteamID()
+	local banMenuItem = {
+		text = "Ban",
+		icon = "icon16/stop.png",
+		subMenu = {}
+	}
+
+	table.insert(banMenuItem.subMenu, { text = "Other (specify)",
+										icon = "icon16/textfield.png",
+										onClick = function(target) OpenBanPlayerDialog(targetName, targetSteamId) end,
+										disableClickTargetValidation = true
+										})
+
+	return { banMenuItem }
+end
+
+
+
+
+
+--[[
+===================================================================================================
+Render functions
+===================================================================================================
+--]]
+
+
+function RenderMenuSections(menu, menuSections)
+
+	for sectionKey, section in pairs(menuSections) do
+		local isFirstSection = sectionKey == 1
+		if not isFirstSection then
+			menu:AddSpacer()
+		end
+		
+		RenderMenuSectionOptions(menu, menu.Player, section)
+	end
+
+end
+
+
+function RenderMenuSectionOptions(menu, target, sectionOptions)
+
+	for _, option in pairs(sectionOptions) do
+		if option.subMenu == nil then
+			local onClick = option.onClick
+
+			if option.disableClickTargetValidation ~= true and onClick ~= nil then
+				if option.onFail == nil then
+					onClick = WrapOnClickPlayer(target, onClick)
+				else
+					onClick = WrapOnClickPlayerWithFailCallback(target, onClick, option.onFail)
+				end
+			end
+
+			AddMenuOption(menu, option.text, option.icon, onClick)
+		else
+			local subMenu = AddSubMenuOption(menu, option.text, option.icon)
+			RenderMenuSectionOptions(subMenu, target, option.subMenu)
+		end
+	end
+
+end
+
+
+function AddMenuOption(menu, text, icon, onClick)
+
+	local menuOption = menu:AddOption(text, function() onClick(menu) end)
+	
+	if not (icon == "" or icon == nil) then 
+		menuOption:SetImage(icon)
+	end
+end
+
+
+function AddSubMenuOption(menu, text, icon)
+
+	local subMenu, parentMenuOption = menu:AddSubMenu(text)
+
+	if not (icon == "" or icon == nil) then 
+		parentMenuOption:SetImage(icon)
+	end
+	
+	return subMenu
+end
+
+
+function WrapOnClickPlayer(target, onClick)
+
+	return function()
+		if IsValid(target) then
+			onClick(target)
+		end
+	end
+end
+
+function WrapOnClickPlayerWithFailCallback(target, onClick, onFail)
+
+	return function()
+		if IsValid(target) then
+			onClick(target)
+		else
+			onFail()
+		end
+	end
+end
+
+
+
+
+
+--[[
+===================================================================================================
+Utility functions
+===================================================================================================
+--]]
+
+function AddMenuSection(menu, section)
+	local isSectionEmpty = next(section) == nil
+	
+	if not isSectionEmpty then
+		table.insert(menu, section)
+	end
+end
+
+
+function PlayerHasRequiredPermission(requiredPermission)
+	return ULib and ULib.ucl.query(LocalPlayer(), requiredPermission)
+end
+
+
 --[[
 ===================================================================================================
 --]]
@@ -134,25 +361,23 @@ function OpenPrivateMessageDialog(target)
 	ShowMessagePanel("Private Message " .. targetName, "Enter a private message to send to " .. targetName, function(message)
 		if IsValid(target) then
 			RunConsoleCommand("ulx", "psay", target:Nick(), message)
-			PlayClickSound()
 		end
 	end)
 end
 
 function OpenKickReasonDialog(target, targetName, failureCallback)
 	ShowMessagePanel("", "Reason for kicking " .. targetName, function(reason)
-		TryRunPlayerMenuOption(target, function(target) RunConsoleCommand("ulx", "kick", target:Nick(), reason) end, failureCallback)
+		WrapOnClickPlayerWithFailCallback(target, function(target) RunConsoleCommand("ulx", "kick", target:Nick(), reason) end, failureCallback)
 	end) 
 end
 
 function OpenBanPlayerDialog(targetName, targetSteamId)
 	ShowMessagePanel("Ban Length", "Length (minutes). 0 is permanent. Use h (hours), d (days) or w (weeks) for different lengths e.g. 1d = 1 day", function(banLength)
 		
-		local validBanLength = string.match(string.lower(string.Trim(banLength)), "^([0-9]+) *([mhdmy])$")
+		local validBanLength = string.match(string.lower(string.Trim(banLength)), "^([0-9]+) *([hdmy]?)$")
 		if validBanLength then
 			ShowMessagePanel("Ban Reason", "Reason for banning " .. targetName, function(reason)
 				RunConsoleCommand("ulx", "banid", targetSteamId, banLength, reason)
-				PlayClickSound()
 			end)
 		else
 			-- TODO: Remove this recursion and handle this better
@@ -161,71 +386,7 @@ function OpenBanPlayerDialog(targetName, targetSteamId)
 		end
 	end)
 end
---[[
-===================================================================================================
---]]
 
-
-
-
-
---[[
-===================================================================================================
-Utility Functions
-===================================================================================================
---]]
-function PlayClickSound()
-	surface.PlaySound("buttons/button9.wav")
-end
-
-function TryRunPlayerMenuOption(target, onSuccess, onError)	
-	if IsValid(target) then
-		onSuccess(target)
-	else
-		onError()
-	end
-end
-
-function AddMenuOption(menu, text, iconImagePath, onClick)
-	local menuOption = menu:AddOption(text, function() PlayClickSound() onClick(menu) end)
-	
-	if not (iconImagePath == "" or iconImagePath == nil) then 
-		menuOption:SetImage(iconImagePath)
-	end
-end
-
-function AddPlayerMenuOption(menu, text, iconImagePath, onClick)
-	AddMenuOption(menu, text, iconImagePath, function()
-		local target = menu.Player
-		
-		if IsValid(target) then
-			onClick(target)
-		end
-	end)
-end
-
-function AddULibRestrictedPlayerMenuOption(menu, text, iconImagePath, requiredULibPermission, onClick)
-	local ply = LocalPlayer() 
-	
-	if ULib and ULib.ucl.query(ply, requiredULibPermission) then
-		AddPlayerMenuOption(menu, text, iconImagePath, onClick)
-	end
-end
-
-function AddULibRestrictedSubMenu(menu, name, iconImagePath, requiredULibPermission)
-	local ply = LocalPlayer()
-	
-	if not (ULib and ULib.ucl.query(ply, requiredULibPermission)) then
-		return nil
-	end
-	
-	local subMenu, parentMenuOption = menu:AddSubMenu(name)
-	if not (iconImagePath == "" or iconImagePath == nil) then 
-		parentMenuOption:SetImage(iconImagePath)
-	end
-	
-	return subMenu
-end
 
 function ShowMessagePanel (panelTitle, inputLabel, successCallback)
 	local curx = panelpadding
